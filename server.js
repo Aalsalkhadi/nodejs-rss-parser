c1e4cbe2ae2752e3bd615ce5d31f9b34897c659b
lupGitHub <aalsalkhadi@lpsp.de>
2022-02-17 20:22:14 +0100

    Add updateDraftTable
    updateDraftTable wurde hinzugefügt, um ein Beispiel dafür zu geben, wie die Hubspot Api und Node.js zusammenarbeiten

--- server.js
+++ server.js
@@ -1,96 +1,74 @@
-var http = require("http");
+const http = require("http");
+const hubspot = require("@hubspot/api-client");
+const hubspotClient = new hubspot.Client({
+  apiKey: "{your-hubspot-api}"
+});
 
-const hubspot = require('@hubspot/api-client');
-
-const hubspotClient = new hubspot.Client({"apiKey":"9b77916b-1d35-4ad3-a309-c781c20615c0"});
-
-const tableId = "5342266";
-const portalId = "9177354";
-var options = [];
-
-
-const tableIdOrName = "5342133";
+const tableIdOrName = "5342466";
 const archived = undefined;
 const includeForeignIds = undefined;
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
+    const dynamicMetaTags = {};
 
-    // function to create a Table
-    // const dynamicMetaTags = {};
-    // const HubDbTableV3Request = {
-    //   name: "test_table_3",
-    //   label: "Test Table_3",
-    //   useForPages: true,
-    //   allowPublicApiAccess: false,
-    //   allowChildTables: true,
-    //   enableChildTablePages: false,
-    //   columns: [
-    //     { name: "text_column", label: "Text Column", id: "1", type: "TEXT" },
-    //   ],
-    //   dynamicMetaTags,
-    // };
-    // async function createTable() {
-    //   try {
-    //     const apiResponse = await hubspotClient.cms.hubdb.tablesApi.createTable(
-    //       HubDbTableV3Request
-    //     );
-    //     console.log(JSON.stringify(apiResponse.body, null, 2));
-    //   } catch (e) {
-    //     e.message === "HTTP request failed"
-    //       ? console.error(JSON.stringify(e.response, null, 2))
-    //       : console.error(e);
-    //   }
-    // }
+    const HubDbTableV3Request = {
+      name: "test",
+      label: "test",
+      useForPages: false,
+      allowPublicApiAccess: false,
+      allowChildTables: false,
+      enableChildTablePages: false,
+      columns: [
+        { id: 2, name: "text_column", label: "text_column", type: "TEXT" },
+        {
+          id: 3,
+          name: "number_column",
+          label: "Number Column",
+          archived: false,
+          type: "NUMBER",
+        },
+        {
+          id: 4,
+          name: "select_column",
+          label: "select_column",
+          archived: false,
+          type: "multiselect",
+          options: [
+            {
+              name: "Option 1",
+              type: "option",
+            },
+          ],
+        },
+        {
+          id: 5,
+          name: "multiselect",
+          label: "Multi Select Column",
+          archived: false,
+          type: "multiselect",
+          options: [
+            {
+              name: "Option 1",
+              type: "option",
+            },
+            {
+              name: "Option 2",
+              type: "option",
+            },
+          ],
+        },
+      ],
+      dynamicMetaTags,
+    };
 
-    // createTable();
-
-
-
-
-
-
-    /* Get details for a published table
-     * Returns the details for the published version of the specified table. This will include the definitions for the columns in the table and the number of rows in the table.
-     * Note: This endpoint can be accessed without any authentication if the table is set to be allowed for public access.
-     */
-
-
-
-    // async function getTableDetails() {
-
-    //   try {
-    //     const apiResponse =
-    //       await hubspotClient.cms.hubdb.tablesApi.getTableDetails(
-    //         tableIdOrName,
-    //         archived,
-    //         includeForeignIds
-    //       );
-    //     console.log(JSON.stringify(apiResponse.body, null, 2));
-    //   } catch (e) {
-    //     e.message === "HTTP request failed"
-    //       ? console.error(JSON.stringify(e.response, null, 2))
-    //       : console.error(e);
-    //   }
-    // }
-
-    // getTableDetails();
-
-
-
-
-
-    /* 
-     * Get details for a draft table
-     * Get the details for the draft version of a specific HubDB table. This will include the definitions for the columns in the table and the number of rows in the table.
-     */
-
-    async function getDraftTableDetailsById() {
+    async function updateDraftTable() {
       try {
         const apiResponse =
-          await hubspotClient.cms.hubdb.draft.tablesApi.getDraftTableDetailsById(
+          await hubspotClient.cms.hubdb.tablesApi.updateDraftTable(
             tableIdOrName,
+            HubDbTableV3Request,
             archived,
             includeForeignIds
           );
@@ -101,14 +79,10 @@ http
           : console.error(e);
       }
     }
+    updateDraftTable();
 
-    console.log(getDraftTableDetailsById());
-
-
-
-    
-
-    res.end("Hello World\n");
+  
+    res.end("Hello!\n");
   })
   .listen(1337, "127.0.0.1");
 
